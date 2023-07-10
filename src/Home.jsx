c40538c8afa491db0187babb60197faabbc610fe
HRpanchal1 <harnishpanchal1512@gmail.com>
2023-07-10 12:56:11 +0530

    react-data

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './Cm-cop/Header';
+
+
+const Home = () => {
+    return (
+        <>
+      
+        <h1>home</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur officia enim tempore nisi fuga sed ex modi officiis iusto saepe odit ducimus sapiente ratione unde rerum laboriosam mollitia laudantium.</p>
+        </>
+      );
+}
+ 
+export default Home;
